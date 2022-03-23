e8f1b4d57fcdd35ba550ccc05246cec02baef3a2
Farhad Madadzade <farhadm1999@gmail.com>
2022-03-23 14:57:56 +0100

    Update mnist.py

--- src/mnist.py
+++ src/mnist.py
@@ -313,7 +313,6 @@ class PytorchMNISTClient(fl.client.Client):
         device: torch.device = torch.device("cpu"),
     ) -> None:
         self.model = MNISTNet().to(device)
-        self.model.load_state_dict(checkpoint)
         self.cid = cid
         self.train_loader = train_loader
         self.test_loader = test_loader
